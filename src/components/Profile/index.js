0bc0d01591e740564a6fd5c3f052131543df57ee
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-30 14:43:10 -0300

    Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Marcio Andrade</h1>
+    <h2>Programador Frontend</h2>
+    <p>
+      Escrevo sobre o desenvolvimento web, novas tecnologias e outras coisas
+      mais.
+    </p>
+  </div>
+)
+
+export default Profile
