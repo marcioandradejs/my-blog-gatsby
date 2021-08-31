7f749bd70e996774ebeda3f522fb45dc97012b03
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-30 23:14:26 -0300

    Social Links component

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Behance } from "@styled-icons/boxicons-logos/Behance"
+import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare"
+import { Youtube } from "@styled-icons/boxicons-logos/Youtube"
+
+const Icons = {
+  Github,
+  Behance,
+  LinkedinSquare,
+  Youtube,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,22 @@
+const handle = "marcioandradejs"
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Behance",
+    url: `https://www.behance.net/${handle}`,
+  },
+  {
+    label: "LinkedinSquare",
+    url: `https://linkedin.com/${handle}`,
+  },
+  {
+    label: "Youtube",
+    url: `https://youtube.com/${handle}`,
+  },
+]
+
+export default links
