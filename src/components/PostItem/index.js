f72e5f67a639051d484fcadbdbb62b2a03fa2c2d
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-31 14:42:20 -0300

    Estruturando os dados do PostItems

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,20 +1,39 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 import * as S from "./styled"
 
-const PostItem = () => (
-  <S.PostItemLink to="/slug/">
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#61728F">DEV</S.PostItemTag>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PortItemInfo>
-        <S.PostItemDate>31 de Agosto de 2021 • 4 min de leitura</S.PostItemDate>
-        <S.PostItemTitle>useState - React Hooks</S.PostItemTitle>
-        <S.PostItemDescription>
-          Manipulando States com useState - React Hooks
-        </S.PostItemDescription>
+        <S.PostItemDate>
+          {date} • {timeToRead}
+        </S.PostItemDate>
+        <S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PortItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
 export default PostItem
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,15 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
+    <PostItem
+      slug="/about"
+      background=""
+      category="DEV"
+      date="31 de Agosto de 2021"
+      timeToRead="4 min de leitura"
+      title="useState - React Hooks"
+      description="Manipulando States com useState - React Hooks"
+    />
   </Layout>
 )
 
