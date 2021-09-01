687c75c7af105982195b40bc00a127ffc0e364dc
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-01 08:49:59 -0300

    ordenando os posts pela data

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,26 +1,26 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
-const BlogPost = () => {
-  const { markdownRemark } = useStaticQuery(graphql`
-    query Post($slug: String) {
-      markdownRemark(fields: { slug: { eq: $slug } }) {
-        frontmatter {
-          title
-        }
-        html
-      }
-    }
-  `)
-
-  const post = markdownRemark
+const BlogPost = ({ data }) => {
+  const post = data.markdownRemark
 
   return (
     <>
-      <h1>{post.frontmatter.title}</h1>
+      <h1>Title: {post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
     </>
   )
 }
+
+export const query = graphql`
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`
 
 export default BlogPost
