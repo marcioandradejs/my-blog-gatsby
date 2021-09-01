e1f9341e123eee9c3d3a0ecfa90c46bbcf71215e
marcioandrade <marcioandrade.dev@gmail.com>
2021-08-31 22:21:11 -0300

    useStaticQuery in blog-post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,26 +1,26 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
-const BlogPost = ({ data }) => {
-  const post = data.markdownRemark
+const BlogPost = () => {
+  const { markdownRemark } = useStaticQuery(graphql`
+    query Post($slug: String) {
+      markdownRemark(fields: { slug: { eq: $slug } }) {
+        frontmatter {
+          title
+        }
+        html
+      }
+    }
+  `)
+
+  const post = markdownRemark
 
   return (
     <>
-      <h1>Title: {post.frontmatter.title}</h1>
+      <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
     </>
   )
 }
-
-export const query = graphql`
-  query Post($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-      }
-      html
-    }
-  }
-`
 
 export default BlogPost
