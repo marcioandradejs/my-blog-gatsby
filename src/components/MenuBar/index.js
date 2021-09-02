15003cab326359780950568c328a7b13c3a810f4
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-01 21:47:26 -0300

    Criando o metodo para trocar de visualizacao

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -6,15 +6,20 @@ import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
 import { Lightbulb as Light } from "@styled-icons/material-outlined/Lightbulb"
 import { Grid } from "@styled-icons/boxicons-solid/Grid"
 import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
+import { ThList as List } from "@styled-icons/fa-solid/ThList"
 
 const MenuBar = () => {
   const [theme, setTheme] = useState(null)
+  const [display, setDisplay] = useState(null)
 
   const isDarkMode = theme === "dark"
+  const isListMode = display === "list"
 
   useEffect(() => {
     setTheme(window.__theme)
+    setDisplay(window.__display)
     window.__onThemeChange = () => setTheme(window.__theme)
+    window.__onDisplayChange = () => setDisplay(window.__display)
   }, [])
 
   return (
@@ -41,8 +46,13 @@ const MenuBar = () => {
         >
           <Light />
         </S.MenuBarItem>
-        <S.MenuBarItem title="Mudar visualização">
-          <Grid />
+        <S.MenuBarItem
+          title="Mudar visualização"
+          onClick={() => {
+            window.__setPreferredDisplay(isListMode ? "grid" : "list")
+          }}
+        >
+          {isListMode ? <Grid /> : <List />}
         </S.MenuBarItem>
         <S.MenuBarItem title="Ir para o topo da página">
           <Arrow />
