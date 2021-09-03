76a1f6753bd326a4ea21af6f1d2d4b0721e31c14
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-03 13:53:36 -0300

    habilitando o Gatsby para funcionar offline(PWA)

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,19 +74,20 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Marcio Andrade`,
+        short_name: `Marcio Andrade`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-sitemap`,
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
